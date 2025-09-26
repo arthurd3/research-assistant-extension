4b78f60b85caaad1264917dcc7cbcd68d49e7c81
arthurd3 <arthur_camposl@yahoo.com>
2025-09-26 11:16:02 -0300

    feat create classes from gemini response

--- research-assistant/src/main/java/com/research/assistant/ResearchService.java
+++ research-assistant/src/main/java/com/research/assistant/ResearchService.java
@@ -1,17 +1,73 @@
 package com.research.assistant;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class ResearchService {
-    public String processContent(final ResearchRequest request) {
-        //Build the prompt
-        //Query the AI API
-        //Parse the response
-        // Return response
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    private final WebClient webClient;
+    private final ObjectMapper objectMapper;
+
+    public ResearchService(WebClient.Builder webClientBuilder, ObjectMapper objectMapper) {
+        this.webClient = webClientBuilder.build();
+        this.objectMapper = objectMapper;
     }
 
-    private String buildPrompt(ResearchRequest request) {
+    public String processContent(final ResearchRequest request) {
+        //Build the prompt
+        String prompt = buildPrompt(request);
+
+        //Query the AI API
+        Map<String , Object> requestBody = Map.of(
+                 "content", new Object[] {
+                    Map.of("parts", new Object[] {
+                            Map.of("text", prompt)
+                    })
+                 }
+        );
+
+        String response = webClient.post()
+                 .uri(geminiApiUrl + geminiApiKey)
+                 .bodyValue(requestBody)
+                 .retrieve()
+                 .bodyToMono(String.class)
+                 .block();
+
+        //Parse the response
+        // Return response
+
+        return extractTextFromResponse(response);
+    }
+
+    private String extractTextFromResponse(final String response) {
+        try {
+            GeminiResponse geminiResponse = objectMapper.readValue(response, GeminiResponse.class);
+            if(geminiResponse.getCandidateList() != null && !geminiResponse.getCandidateList().isEmpty()) {
+                GeminiResponse.Candidate firstCandidate = geminiResponse.getCandidateList().getFirst();
+                if(firstCandidate.getContent() != null
+                        && firstCandidate.getContent().getParts() != null
+                        && !firstCandidate.getContent().getParts().isEmpty()
+                ){
+                    return firstCandidate.getContent().getParts().getFirst().getText();
+                }
+            }
+        }catch (Exception e) {
+            return "Error Parsing Response" + e.getMessage();
+        }
+    }
+
+    private String buildPrompt(final ResearchRequest request) {
         StringBuilder prompt = new StringBuilder();
         switch (request.getOperation()) {
             case "summarize":
